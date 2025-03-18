15446060e185da22dbc159ea0fd44780e8a151bb
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-18 16:24:24 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: false,
       builder: (context) => MyApp(),
     ),
   );
@@ -28,14 +29,56 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
           child: Column(
             children: [
               // Remove All
+              Row(
+                children: [
+                  Spacer(),
+                  CupertinoButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {},
+                    child: Text(
+                      "Remove All",
+                      style: TextStyle(
+                        color: Color(0xff272727),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
 
               // Items (2x)
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color(0xffF4F4F4),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "assets/img1.png",
+                      height: 64,
+                      width: 64,
+                    ),
+                    SizedBox(width: 12),
+                    Column(
+                      children: [
+                        Text(
+                          "Men's Harrington Jacket",
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
 
               Spacer(),
 
