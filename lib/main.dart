dd83f41c42c5d4f0620da0ea86bae7287b4118be
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-18 16:09:44 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              // Remove All
+
+              // Items (2x)
+
+              Spacer(),
+
+              // Prices
+
+              // Coupon
+
+              // Button
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
